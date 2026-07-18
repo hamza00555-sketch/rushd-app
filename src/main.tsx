d5ce80608ed61a9c4f0466c9157cc3e1c83c4bb7
hamza00555-sketch <hamza00555@gmail.com>
2026-07-18 10:51:49 +0300

    style(sprint-05): load promotion simulator styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './styles.css'
 import './sprint02.css'
 import './sprint03.css'
 import './sprint04.css'
+import './sprint05.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
